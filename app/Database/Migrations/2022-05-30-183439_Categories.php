94f56fb8388449e96398fe441350a1e561bdb527
erick243cd <erickbanze.develo@gmail.comd>
2022-05-30 12:11:56 -0700

    Up function

--- app/Database/Migrations/2022-05-30-183439_Categories.php
+++ app/Database/Migrations/2022-05-30-183439_Categories.php
@@ -8,7 +8,27 @@ class Categories extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'categryId'            => [
+                'type'           => 'INT',
+                'auto_increment' => TRUE
+            ],
+            'categoryName_fr'      => [
+                'type'          => 'VARCHAR',
+                'constraint'    => '255'
+            ],
+            'categoryName_en'      => [
+                'type'          => 'VARCHAR',
+                'constraint'    => '255'
+            ],
+            'isactive'      => [
+                'type'          => 'INTEGER',
+                'default'       => 1
+            ]
+        ]);
+    
+        $this->forge->addKey('categryId', TRUE);
+        $this->forge->createTable('pl_categories');
     }
 
     public function down()
