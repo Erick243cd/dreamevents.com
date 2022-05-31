a429ea42c12c587960937a65edefbae3e4198e90
erick243cd <erickbanze.develo@gmail.comd>
2022-05-31 11:33:12 -0700

    Services controller

---
+++ app/Controllers/Services.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use Config\UserAgents;
+
+class Services extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function servicesByCategories(?string $slug = null)
+    {
+        $category = $this->categoreyModel->asObject()->where('categorySlug', $slug)->first();
+
+        $agent = $this->request->getUserAgent();
+
+        $page = "listbycategory";
+
+        if (!empty($category)) {
+            $data = [
+                'services' => $this->serviceModel->asObject()->where('categoryId', $category->categoryId)->findAll(),
+                'links' => headerData($page, localLang()),
+                'page' => $page,
+                'lang' => localLang(),
+                'isMobile' => $agent->isMobile(),
+                'categories' =>  categoryData($page),
+                'cities' => cityData($page)
+            ];
+
+            return view('services/'. localLang().'/servicesbycategory', $data);
+        } else {
+            return view('error/404');
+        }
+    }
+}
