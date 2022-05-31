c33d865c9b4d928f459f3a80db9a5d8e6ae4b204
erick243cd <erickbanze.develo@gmail.comd>
2022-05-31 14:49:55 -0700

    services data

--- app/Helpers/store_helper.php
+++ app/Helpers/store_helper.php
@@ -2,16 +2,17 @@
 
 use App\Models\CategoryModel;
 use App\Models\CityModel;
+use Config\UserAgent;
 
 function categoryData(?string $page)
 {
     $category =  new CategoryModel();
-    $limit = $page === 'services' ? 10 : 4;
+    $limit = $page === 'services' ? 10 : 6;
     return $category->asObject()->limit($limit)->find();
 }
 
 function cityData(?string $page){
     $city = new CityModel();
-    $limit = $page === 'services' ? 10 : 4;
+    $limit = $page === 'services' ? 10 : 6;
     return $city->asObject()->limit($limit)->find();
 }
