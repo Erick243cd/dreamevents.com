c508cfb10b96108c0f7a5f507e8af7d1be3f0ee4
erick243cd <erickbanze.develo@gmail.comd>
2022-06-06 13:55:12 -0700

    Services by category data

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -8,20 +8,25 @@ class Pages extends BaseController
 {
     public function views($page = 'home')
     {
+      
         $agent = $this->request->getUserAgent();
 
         if (file_exists(APPPATH . 'views/pages/' . localLang() . '/' . $page . '.php')) {
             //Head and footer Links
-            
+
             $data = [
                 'links' => headerData($page, localLang()),
                 'page' => $page,
                 'lang' => localLang(),
                 'isMobile' => $agent->isMobile(),
                 'categories' =>  categoryData($page),
-                'cities'=>cityData($page)
+                'allcategories' => allCategoriesData(),
+                'cities' => cityData($page),
+                'premiumservices' => $page === 'home' ? premiumServices($page) : null,
+                'allservices' => $page === 'home' ? allServices($page) : null,
+                'servicesByCity' => $page === 'home' ? servicesByCity() : null
             ];
-            
+
             return view('pages/' . localLang() . '/' . $page, $data);
         } else {
             return view('errors/404');
