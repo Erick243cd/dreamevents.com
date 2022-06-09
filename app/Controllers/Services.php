8f5d312096b6eeff2706f9589e1459bd88531c0d
erick243cd <erickbanze.develo@gmail.comd>
2022-06-09 08:37:17 -0700

    Services Controller updated

--- app/Controllers/Services.php
+++ app/Controllers/Services.php
@@ -7,33 +7,62 @@ use Config\UserAgents;
 
 class Services extends BaseController
 {
-    public function index()
+    protected $isMobile;
+    protected $page;
+    protected $agent;
+
+
+    public function __construct()
     {
-        //
+        $request = \Config\Services::request();
+        $this->agent = $request->getUserAgent();
+        $this->page = "listbycategory";
+        $this->isMobile = $this->agent->isMobile();
     }
+
 
     public function servicesByCategories(?string $slug = null)
     {
         $category = $this->categoryModel->asObject()->where('categorySlug', $slug)->first();
 
-        $agent = $this->request->getUserAgent();
-
-        $page = "listbycategory";
-
         if (!empty($category)) {
             $data = [
                 'services' => $this->serviceModel->asObject()->where('categoryId', $category->categoryId)->findAll(),
-                'links' => headerData($page, localLang()),
-                'page' => $page,
+                'links' => headerData($this->page, localLang()),
+                'page' => $this->page,
                 'lang' => localLang(),
-                'isMobile' => $agent->isMobile(),
-                'categories' =>  categoryData($page),
-                'cities' => cityData($page)
+                'isMobile' => $this->isMobile,
+                'categories' =>  categoryData($this->page),
+                'cities' => cityData($this->page),
             ];
 
-            return view('services/'. localLang().'/servicesbycategory', $data);
+            return view('services/' . localLang() . '/servicesbycategory', $data);
         } else {
             return view('errors/404');
         }
+    }
+
+    // Search function
+    public function serviceSearch()
+    {
+        $city = $this->request->getVar('city');
+        $category = $this->request->getVar('category');
+        $wheres = $category == "all" ? ["lp_services.cityId" => $city] : ["lp_services.cityId" => $city, "lp_services.categoryId" => $category];
+
+        $data = [
+            'links' => headerData($this->page, localLang()),
+            'page' => $this->page,
+            'lang' => localLang(),
+            'isMobile' => $this->isMobile,
+            'categories' =>  categoryData($this->page),
+            'allcategories' => allCategoriesData(),
+            'cities' => cityData($this->page),
+            'premiumservices' => premiumServices($this->page),
+            'searchResult' => $this->serviceModel->asObject()
+                ->join('lp_cities', 'lp_services.cityId=lp_cities.cityId')
+                ->join('lp_categories', 'lp_categories.categoryId=lp_services.serviceId')
+                ->where($wheres)->orderBy('serviceName_fr', 'ASC')->find()
+        ];
+        return view('services/' . localLang() . '/listing', $data);
     }
 }
